0cc641fd74955b20bc38ea0fcb6edc228701dc54
Andrey <medvedevandrey860@gmail.com>
2025-10-29 02:32:45 +0200

    create dto and errors user_service

---
+++ internal/service/product/user/dto.go
@@ -0,0 +1,33 @@
+package user
+
+import models "github.com/Xiancel/ecommerce/internal/domain"
+
+type CreateUserRequest struct {
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=6,max=100"`
+	FirstName string `json:"first_name" validate:"required,min=2,max=100"`
+	LastName  string `json:"last_name" validate:"required,min=2,max=100"`
+	Role      string `json:"role" validate:"omitempty,oneof=user admin"`
+}
+
+type UpdateUserRequest struct {
+	Email     *string `json:"email" validate:"omitempty,email"`
+	Password  *string `json:"password" validate:"omitempty,min=6,max=100"`
+	FirstName *string `json:"first_name" validate:"omitempty,min=2,max=100"`
+	LastName  *string `json:"last_name" validate:"omitempty,min=2,max=100"`
+	Role      *string `json:"role" validate:"omitempty,oneof=user admin"`
+}
+
+type UserFilter struct {
+	Search  string  `json:"search"`
+	Role    *string `json:"role" validate:"omitempty,oneof=user admin"`
+	Limit   int     `json:"limit" validate:"required,min=1,max=100"`
+	Offset  int     `json:"offset" validate:"gte=0"`
+	OrderBy string  `json:"order_by" validate:"omitempty,oneof=created_at_asc created_at_desc name_asc name_desc"`
+}
+type UserListResponse struct {
+	Users  []*models.User `json:"users"`
+	Total  int            `json:"total"`
+	Limit  int            `json:"limit"`
+	Offset int            `json:"offset"`
+}
